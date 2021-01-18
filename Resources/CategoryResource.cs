7e4c0a6c4667f1982ea4755a3fa5cfd71c19aec3
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2021-01-18 13:06:18 +0530

    Add resources

---
+++ Resources/CategoryResource.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Supermarket.API.Resources
+{
+    public class CategoryResource
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
---
+++ Resources/SaveCategoryResource.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Supermarket.API.Resources
+{
+    public class SaveCategoryResource
+    {
+        [Required]
+        [MaxLength(30)]
+        
+        public string Name { get; set; }
+
+
+    }
+}
