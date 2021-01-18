589a93fa227001fdf002ec595281b2bad63d3c42
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2021-01-18 13:06:04 +0530

    Add Mapping

---
+++ Mapping/ResourceToModelProfile .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Supermarket.API.Resources;
+using Supermarket.API.Domain.Models;
+using AutoMapper;
+
+
+namespace Supermarket.API.Mapping
+{
+    public class ResourceToModelProfile: Profile
+    {
+        public ResourceToModelProfile()
+        {
+            CreateMap<SaveCategoryResource, Category>(); 
+        }
+    }
+}
