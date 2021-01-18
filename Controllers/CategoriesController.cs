3884cc361d26a5dd83d1c631165a5009b20bb3cf
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2021-01-18 13:21:00 +0530

    removed unneccessary files

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -21,7 +21,7 @@ namespace Supermarket.API.Controllers
             _mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet]  
         public async Task<IEnumerable<Category>> GetAllAsync()
         {
             var categories = await _categoryService.ListAsync();
